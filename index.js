384dd1e8abcda0ffa126dd4329b85d5aa8e57e3b
entryone <a.i.kulinich@gmail.com>
2022-06-29 21:51:37 +0300

    5555

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-// run `node index.js` in the terminal
+const path = require('path');
+const MTProto = require('@mtproto/core');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const api_id = '19841528';
+const api_hash = '266be745bc0ed86ff5b5226488cff50a';
+
+// 1. Create instance
+const mtproto = new MTProto({
+  api_id,
+  api_hash,
+
+  storageOptions: {
+    path: path.resolve(__dirname, './data/1.json'),
+  },
+});
+
+// 2. Print the user country code
+mtproto.call('help.getNearestDc').then(result => {
+  console.log('country:', result.country);
+});
